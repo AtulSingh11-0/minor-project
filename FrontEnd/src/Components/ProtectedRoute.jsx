ce8ee1169ebaf1b4563c335a3f0f5ae01c234855
Simran Summan <summansimran7@gmail.com>
2024-12-14 20:08:05 +0530

    updated protected route.jsx

--- FrontEnd/src/Components/ProtectedRoute.jsx
+++ FrontEnd/src/Components/ProtectedRoute.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { useAuth } from "../Hooks/AuthCheck";
+import PropTypes from "prop-types";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth();
-  return localStorage.getItem("jwt") ? <Outlet /> : <Navigate to="/login" />;
+const ProtectedRoute = ({ allowedRoles }) => {
+  const isLoggedIn = Boolean(localStorage.getItem("token"));
+  const role = localStorage.getItem("role");
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />; // Redirect to login if not logged in
+  }
+
+  if (!allowedRoles.includes(role)) {
+    // Redirect based on role
+    return <Navigate to={role === "admin" ? "/admin-home" : "/home"} />;
+  }
+
+  return <Outlet />; // Render nested routes
+};
+
+ProtectedRoute.propTypes = {
+  allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default ProtectedRoute;
